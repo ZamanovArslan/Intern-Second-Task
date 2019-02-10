d002a55495626b181c3d25daa15f2c21849194e9
LionZam <lionzamanov@gmail.com>
2019-02-10 18:34:28 +0300

    log in change

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,8 @@
 class Event < ApplicationRecord
+  validates :name_of_event, presence: true
+  validates :regularity, presence: true
+  validates :date_event, presence: true
+
   def self.is_available(date, id)
     # LANGUAGE = SQL
     find_by_sql("SELECT *
